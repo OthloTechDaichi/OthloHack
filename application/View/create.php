c3a27ca82bc17f17c1e85c38c19fe4d5fd61fc45
x17018xx <x17018xx@pcx17018xx.local>
2017-10-07 16:55:39 +0900

    家事の選択画面作成

--- application/View/create.php
+++ application/View/create.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
+   <!-- <title></title> -->
+
+   <!-- Bootstrap core CSS -->
+   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+   <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+   <!-- Custom styles for this template -->
+   <link href="../../css/base.css" rel="stylesheet">
+   <!-- <link rel="shortcut icon" href="../assets/images/favicon.ico"> -->
+   <!-- <link rel="apple-touch-icon-precomposed" href="../assets/images/favicon.png"> -->
+ </head>
+ <body>
+   <h1>カテゴリ</h1>
+   <div class="container">
+     <div class="row">
+       <div class="col-6"><a href="#" class="button kitchen"></a></div>
+       <div class="col-6"><a href="#" class="button wash"></a></div>
+     </div>
+     <div class="row">
+       <div class="col-6"><a href="#" class="button shop"></a></div>
+       <div class="col-6"><a href="#" class="button clean"></a></div>
+     </div>
+   </div>
+ </body>
+</html>
