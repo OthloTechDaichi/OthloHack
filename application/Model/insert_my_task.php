dea17308154ff00b316ed5f4a37cb0efa89bf1b7
Yusuke Suzuki <cs16047@s.inf.shizuoka.ac.jp>
2017-10-08 10:58:43 +0900

    タスクをmy_task_listに追加できるようにした

--- application/Model/insert_my_task.php
+++ application/Model/insert_my_task.php
@@ -4,7 +4,7 @@ include 'ChromePhp.php';
     $sub_category_id = $_POST['sub_category_id'];
     date_default_timezone_set('Asia/Tokyo');
     $date = date("Y-m-d");
-    $db = new SQLite3('../db/daich.db');
+    $db = new SQLite3('../../db/daich.db');
     $stmt = $db->prepare("INSERT INTO my_task_list(category_id, sub_category_id,date_time,done) VALUES (?,?,?,'0')");
     $stmt->bindValue(1, $category_id, SQLITE3_INTEGER);
     $stmt->bindValue(2, $sub_category_id, SQLITE3_INTEGER);
